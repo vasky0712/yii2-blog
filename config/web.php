928cfe0adff77d67dd2b3c4a9f8b77bba547e8be
vasky0712 <vadimca37111@mail.ru>
2019-10-30 21:58:02 +0500

    Создал модуль, модели для таблиц и CRUD под статьи, теги, категории

--- config/web.php
+++ config/web.php
@@ -52,6 +52,11 @@ $config = [
         ],
         
     ],
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+    ],
     'params' => $params,
 ];
 
---
+++ models/Comment.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "comment".
+ *
+ * @property int $id
+ * @property string $text
+ * @property int $userId
+ * @property int $articleId
+ * @property int $status
+ *
+ * @property Article $article
+ * @property User $user
+ */
+class Comment extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'comment';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['userId', 'articleId', 'status'], 'integer'],
+            [['text'], 'string', 'max' => 255],
+            [['articleId'], 'exist', 'skipOnError' => true, 'targetClass' => Article::className(), 'targetAttribute' => ['articleId' => 'id']],
+            [['userId'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['userId' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'text' => 'Text',
+            'userId' => 'User ID',
+            'articleId' => 'Article ID',
+            'status' => 'Status',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticle()
+    {
+        return $this->hasOne(Article::className(), ['id' => 'articleId']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'userId']);
+    }
+}
